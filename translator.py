d7e1e124e2b85baacb2f6bb267e99ed2b6b967f5
atnartur <i@atnartur.ru>
2022-09-27 16:25:27 +0400

    + translation - functions

--- translator.py
+++ translator.py
@@ -14,7 +14,8 @@ REVERSED_DICT = {}
 for key, value in DICT.items():
     REVERSED_DICT[value] = key
 
-while True:
+
+def get_parameters():
     word = input("Введите слово для перевода: ")
 
     print("Выберите направление перевода")
@@ -24,16 +25,33 @@ while True:
     if not direction:
         direction = 1
     direction = int(direction)
+    return word, direction
+
+
+def get_translation(word, dictionary):
+    if word in dictionary:
+        return dictionary[word]
+    else:
+        return None
+
+
+def print_translation(word, translation):
+    if translation:
+        print(f"Перевод слова {word} - {translation}")
+    else:
+        print(f"Нет перевода для слова {word}")
+
+
+def prepare_word(word):
+    return word.lower().strip()
+
+
+while True:
+    word, direction = get_parameters()
+    word = prepare_word(word)
 
     if direction == 1:
-        if word in DICT:
-            translation = DICT[word]
-            print(f"Перевод слова {word} - {translation}")
-        else:
-            print(f"Нет перевода для слова {word}")
+        translation = get_translation(word, DICT)
     else:
-        if word in REVERSED_DICT:
-            translation = REVERSED_DICT[word]
-            print(f"Перевод слова {word} - {translation}")
-        else:
-            print(f"Нет перевода для слова {word}")
+        translation = get_translation(word, REVERSED_DICT)
+    print_translation(word, translation)
