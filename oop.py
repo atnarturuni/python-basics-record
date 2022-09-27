4c2ed964a3cf291537682d368bb0792fd5c64ae7
atnartur <i@atnartur.ru>
2022-09-27 19:37:11 +0400

    + oop - incapsulation

--- oop.py
+++ oop.py
@@ -2,18 +2,24 @@ class Cat():
     _count = 0
 
     def __init__(self, name, weight, color):
-        self.name = name
+        self.__name = name
         self.weight = weight
         self.color = color
         self.fullness = 0
         Cat._count += 1
 
     def eat(self, fullness=1):
-        print(f"{self.name} поел")
+        print(f"{self.__name} поел")
         self.fullness += fullness
 
     def __str__(self):
-        return f'Кот {self.name}'
+        return f'Кот {self.__name}'
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
 
     @staticmethod
     def get_count():
@@ -23,12 +29,14 @@ class Cat():
 vasya = Cat("Вася", 5, "black")
 barsik = Cat("Барсик", 8, "ginger")
 
-print(vasya.name, vasya.color)
-print(barsik.name, barsik.color, barsik.fullness)
+# print(vasya.name, vasya.color)
+# print(barsik.name, barsik.color, barsik.fullness)
 barsik.eat(2)
 barsik.eat()
 barsik.eat()
 print(barsik.fullness)
 print(vasya)
+vasya.set_name("Вася великолепный")
+print(vasya.get_name())
 
 print(Cat.get_count())
