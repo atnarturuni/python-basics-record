d2b198b1fd26c02323d57d594e6b4ae866a877cd
atnartur <i@atnartur.ru>
2022-09-27 19:49:44 +0400

    + principles

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-class Cat():
+class Animal():
     _count = 0
 
     def __init__(self, name, weight, color):
@@ -6,14 +6,14 @@ class Cat():
         self.weight = weight
         self.color = color
         self.fullness = 0
-        Cat._count += 1
+        Animal._count += 1
 
     def eat(self, fullness=1):
         print(f"{self.__name} поел")
         self.fullness += fullness
 
     def __str__(self):
-        return f'Кот {self.__name}'
+        return f'Животное {self.__name}'
 
     def get_name(self):
         return self.__name
@@ -21,9 +21,23 @@ class Cat():
     def set_name(self, name):
         self.__name = name
 
+    def say(self):
+        print(f'{self.__name} говорит!')
+
     @staticmethod
     def get_count():
         return Cat._count
+
+
+class Cat(Animal):
+    def __str__(self):
+        return f'Кот {self.get_name()}'
+
+    def say(self):
+        print(f'{self.get_name()}: мяу!')
+
+    def mur(self):
+        print(f'{self.get_name()}: мур!')
 
 
 vasya = Cat("Вася", 5, "black")
@@ -38,5 +52,40 @@ print(barsik.fullness)
 print(vasya)
 vasya.set_name("Вася великолепный")
 print(vasya.get_name())
+vasya.say()
+vasya.mur()
 
 print(Cat.get_count())
+
+parrot = Animal("Кеша", 3, "red")
+print(parrot)
+parrot.say()
+
+
+class Device():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Phone(Device):
+    def make_a_call(self, phone_number):
+        print(f"Звоним на {phone_number}")
+
+    def take_a_photo(self):
+        print(f"{self.name}: фотография!")
+
+
+class PhotoCamera(Device):
+    def take_video(self):
+        print('записывает видео')
+
+    def take_a_photo(self):
+        print(f"{self.name}: фотография!")
+
+
+objs = [Phone("samsung"), PhotoCamera("canon")]
+for obj in objs:
+    obj.take_a_photo()
